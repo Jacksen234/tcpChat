c4dba19c3a1c087b8a4acb5c36d4de397500511d
Jakob <jkohla03@gmail.com>
2021-03-10 23:30:34 +0100

    Finished basic functionality (BUG: Colours double)

--- server/public/client.js
+++ server/public/client.js
@@ -4,6 +4,8 @@ const ownUsernameDisplay = document.querySelector('#own-username');
 
 const inputPanel = document.querySelector('#input-panel');
 const chatPanel = document.querySelector('#chat-panel');
+const chatContainer = document.querySelector('#chat-container');
+const messageInput = document.querySelector('#message-input');
 
 const joinRoomButton = document.querySelector('#join-room');
 const createRoomButton = document.querySelector('#create-room');
@@ -16,14 +18,13 @@ let warningTimeout;
 const socket = io();
 
 let connected = false;
-let typing = false;
 let roomName = '';
 let username = '';
 
 
 joinRoomButton.addEventListener('click', () => {
     if (usernameInput.value && roomID.value){
-        joinChatRoom()
+        joinChatRoom(usernameInput.value, roomID.value);
     }else{
         showWarning('Please provide a username and a roomID');
     }
@@ -31,18 +32,30 @@ joinRoomButton.addEventListener('click', () => {
 
 createRoomButton.addEventListener('click', () => {
     if (usernameInput.value){
-        createChatRoom();
+        createChatRoom(usernameInput.value);
     }else{
         showWarning('Please provide a username');
     }
 });
 
-function joinChatRoom() {
-    
+messageInput.addEventListener('keydown', (ev) => {
+    if (ev.keyCode === 13){
+        if (messageInput.value){
+            socket.emit('sendMessage', messageInput.value);
+            messageInput.value = '';
+        }
+    }
+});
+
+function joinChatRoom(usr, room) {
+    socket.emit('joinRoom', {
+        username: usr,
+        roomID: room
+    });
 }
 
-function createChatRoom() {
-    socket.emit('createRoom', usernameInput.value);
+function createChatRoom(usr) {
+    socket.emit('createRoom', usr);
 }
 function showWarning(warn) {
     clearTimeout(warningTimeout);
@@ -54,8 +67,78 @@ function showWarning(warn) {
     }, 3000);
 }
 
-socket.on('tooManyRooms', () => {
-    showWarning('Too many rooms');
+function appendMessageLine(type, data) {
+    let htmlString = '';
+    switch (type) {
+        case 'info': {
+            htmlString = createInfoMessage(data.infoText);
+            break;
+        }
+        case 'userJoined': {
+            htmlString = createJoinMessage(data.joinedUser, data.numUsers);
+            break;
+        }
+        case 'userLeft': {
+            htmlString = createLeaveMessage(data.leftUser, data.numUsers);
+            break;
+        }
+        case 'ownMessage': {
+            htmlString = createOwnMessage(data.messageContent, data.timestamp);
+            break;
+        }
+        case 'messageReceived': {
+            htmlString = createReceivedMessage(data.messageContent, data.userReceived, data.timestamp, data.userColour);
+        }
+    }
+    chatContainer.innerHTML += htmlString;
+}
+
+function createInfoMessage(infoText) {
+    return `<div class="msg-line center">
+                <p class="msg-text">
+                    <span class="info">${infoText}</span>
+                </p>
+            </div>`;
+}
+
+function createJoinMessage(joinedUser, numUsers) {
+    return `<div class="msg-line center">
+                <p class="msg-text">
+                    <span class="user-joined">${joinedUser} joined</span>
+                    <span class="info"> --> Current participants: ${numUsers}</span>
+                </p>
+            </div>`;
+}
+function createLeaveMessage(leftUser, numUsers) {
+    return `<div class="msg-line center">
+                <p class="msg-text">
+                    <span class="user-left">${leftUser} left</span>
+                    <span class="info"> --> Current participants: ${numUsers}</span>
+                </p>
+            </div>`;
+}
+
+function createOwnMessage(msg, timestamp) {
+    return `<div class="msg-line right">
+                <p class="msg-text">
+                    <span class="sent">${msg}</span>
+                    <span class="timestamp-received info">${timestamp}</span>
+                </p>
+            </div>`;
+}
+
+function createReceivedMessage(msg, usr, timestamp, col) {
+    return `<div class="msg-line received">
+                <p class="msg-text">
+                    <span class="timestamp-received info">${timestamp}</span>
+                    <span class="username-received" style="color: ${col}">${usr}: </span>
+                    <span class="sent">${msg}</span>
+                </p>
+            </div>`;
+}
+
+socket.on('warning', (str) => {
+    showWarning(str);
 });
 
 socket.on('joinRoom', (data) => {
@@ -64,5 +147,50 @@ socket.on('joinRoom', (data) => {
     inputPanel.style.display = 'none';
     chatPanel.style.display = 'flex';
     roomIDDisplay.innerText = `Room ID: ${data.roomID}`;
-    ownUsernameDisplay.innerText = `${username}@${roomName}: `;
+    ownUsernameDisplay.innerText = `${username}: `;
+    ownUsernameDisplay.style.color = data.userColour;
+    connected = true;
+    appendMessageLine('info', {
+        infoText: `== == == == Welcome to chat room ${roomName} == == == ==`
+    });
+    appendMessageLine('info', {
+        infoText: `Current participants: ${data.currentUsers}`
+    });
 });
+socket.on('userJoined', (data) => {
+    appendMessageLine('userJoined', {
+        joinedUser: data.username,
+        numUsers: data.currentUsers
+    });
+    scrollToBottom();
+});
+
+socket.on('userLeft', (data) => {
+    appendMessageLine('userLeft', {
+        leftUser: data.username,
+        numUsers: data.currentUsers
+    });
+    scrollToBottom();
+});
+
+socket.on('ownMessage', (data) => {
+    appendMessageLine('ownMessage', {
+        messageContent: data.messageContent,
+        timestamp: data.timestamp,
+    });
+    scrollToBottom();
+});
+
+socket.on('messageReceived', (data) => {
+    appendMessageLine('messageReceived', {
+        messageContent: data.messageContent,
+        userReceived: data.username,
+        timestamp: data.timestamp,
+        userColour: data.userColour
+    });
+    scrollToBottom();
+});
+
+function scrollToBottom(){
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+}
--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 const maxRooms = 10;
 let activeRooms = 0;
+let roomUserCount = {};
+let usedColours = {};
 
 server.listen(PORT, () => {
     console.log(`TCP Socket listening on port ${PORT}...`);
@@ -14,26 +16,83 @@ server.listen(PORT, () => {
 
 app.use(express.static(path.join(__dirname, "public")));
 
-
 io.on('connection', (socket) => {
     let addedUser = false;
+    let roomID = '';
+    let userColour = '';
 
     socket.on('createRoom', (username) => {
         if (addedUser) return;
         if(activeRooms >= maxRooms){
-            return socket.emit('tooManyRooms');
+            return socket.emit('warning', 'Too many rooms');
         }
-        socket.username = username;
+        socket.username = username.length > 18 ? 'Herr Lo' : username;
         addedUser = true;
-        const roomID = generateRoomID(3);
+        roomID = generateRoomID(3);
         socket.join(roomID);
         activeRooms++;
+        roomUserCount[roomID] = 1;
+        usedColours[roomID] = [];
+        userColour = getRandomColour(roomID);
         socket.emit('joinRoom', {
             roomID,
-            username: socket.username
+            username: socket.username,
+            currentUsers: 1,
+            userColour
         });
     });
 
+    socket.on('joinRoom', (data) => {
+        if (addedUser) return;
+        if(!io.sockets.adapter.rooms.has(data.roomID)){
+            return socket.emit('warning', 'Room does not exist');
+        }
+        if (roomUserCount[data.roomID] >= 9){
+            return socket.emit('warning', 'Room ist full');
+        }
+        roomID = data.roomID;
+        socket.username = data.username.length > 18 ? 'Herr Lo' : data.username;
+        addedUser = true;
+        roomUserCount[roomID]++;
+        userColour = getRandomColour(roomID);
+        socket.join(data.roomID);
+        socket.broadcast.to(roomID).emit('userJoined', {
+            username: socket.username,
+            currentUsers: roomUserCount[roomID],
+            userColour
+        });
+        socket.emit('joinRoom', {
+            roomID,
+            username: socket.username,
+            currentUsers: roomUserCount[roomID],
+            userColour
+        });
+    });
+
+    socket.on('sendMessage', (msg) => {
+        let timestamp = getCurrentTimeStamp();
+        socket.broadcast.to(roomID).emit('messageReceived', {
+            username: socket.username,
+            messageContent: msg,
+            timestamp,
+            userColour
+        });
+        socket.emit('ownMessage', {
+            messageContent: msg,
+            timestamp
+        });
+    });
+
+    socket.on('disconnect', () => {
+        if (addedUser) {
+            roomUserCount[roomID]--;
+            socket.broadcast.to(roomID).emit('userLeft', {
+                username: socket.username,
+                currentUsers: roomUserCount[roomID]
+            });
+            socket.leave(roomID);
+        }
+    });
 });
 
 function generateRoomID(len) {
@@ -42,5 +101,19 @@ function generateRoomID(len) {
     for (let i = 0; i < len; i++) {
         res += pool.charAt(Math.floor(Math.random() * pool.length));
     }
+    return res;
+}
+
+function getCurrentTimeStamp() {
+    const date = new Date();
+    return `${date.getHours() >= 10 ? date.getHours() : '0' + date.getHours()}:${date.getMinutes() >= 10 ? date.getMinutes() : '0' + date.getMinutes()}`;
+}
+
+function getRandomColour(id) {
+    const colours = ['#FFF100', '#ff8c00', '#e81123', '#ec008c', '#68217a', '#00bcf2', '#00b294', '#009e49', '#bad80a'];
+    let res = '';
+    do {
+        res = colours[Math.floor(Math.random() * colours.length)];
+    } while(usedColours[id].includes(res));
     return res;
 }
